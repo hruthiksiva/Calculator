8f822c0146fa4ee31c30f4a286958b305cca1cc3
Hruthik S <team.hruthiksivakumar@gmail.com>
2020-08-02 09:35:52 +0530

    Added Vibrations

--- app/src/main/java/com/hruthik/calculator/MainActivity.kt
+++ app/src/main/java/com/hruthik/calculator/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.hruthik.calculator
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.os.Vibrator
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import net.objecthunter.exp4j.ExpressionBuilder
 
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,61 +18,92 @@ class MainActivity : AppCompatActivity() {
         var countsign: Int = 0
         var dotcount: Int = 0
         var dividesign: Int = 0
+
         /*Number Buttons*/
         one.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign = 0
             dividesign = 0
             evaluateExpression("1", clear = true)
         }
 
         two.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign = 0
             dividesign = 0
             evaluateExpression("2", clear = true)
         }
 
         three.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign = 0
             dividesign = 0
             evaluateExpression("3", clear = true)
         }
         four.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign = 0
             dividesign = 0
             evaluateExpression("4", clear = true)
         }
 
         five.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign = 0
             dividesign = 0
             evaluateExpression("5", clear = true)
         }
 
         six.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign = 0
             dividesign = 0
             evaluateExpression("6", clear = true)
         }
 
         seven.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign = 0
             dividesign = 0
             evaluateExpression("7", clear = true)
         }
 
         eight.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign = 0
             dividesign = 0
             evaluateExpression("8", clear = true)
         }
 
         nine.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign = 0
             dividesign = 0
             evaluateExpression("9", clear = true)
         }
 
         zero.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign = 0
             if(dividesign==0)
                  evaluateExpression("0", clear = true)
@@ -78,24 +112,36 @@ class MainActivity : AppCompatActivity() {
         /* Operators*/
 
         add.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign++
             if (countsign == 1)
                 evaluateExpression("+", clear = true)
         }
 
         minus.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign++
             if (countsign == 1)
                 evaluateExpression("-", clear = true)
         }
 
         multiply.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign++
             if (countsign == 1)
                 evaluateExpression("*", clear = true)
         }
 
         divide.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign++
             dividesign++
             if (countsign == 1)
@@ -103,6 +149,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         dot.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             countsign++
             dotcount++
             if (countsign == 1 && dotcount==1)
@@ -110,10 +159,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         clear.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             expression.text = ""
             result.text = ""
         }
         equals.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             try {
 
                 if (countsign == 0 && expression.text.toString() != "") {
@@ -139,6 +194,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         delete.setOnClickListener {
+            val vibratorService = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).also {
+                it.vibrate(100)
+            }
             val texts = expression.text.toString()
             if (texts.isNotEmpty()) {
                 expression.text = texts.dropLast(1)
@@ -163,4 +221,12 @@ class MainActivity : AppCompatActivity() {
             result.text = ""
         }
     }
+    /*fun Fragment.vibratePhone() {
+        val vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= 26) {
+            vibrator.vibrate(VibrationEffect.createOneShot(20, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            vibrator.vibrate(20)
+       } }*/
+
 }
